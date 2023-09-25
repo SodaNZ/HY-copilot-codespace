73b60627366635affe665079aac54f9691d5a14f
SodaNZ <SodaNZ@users.noreply.github.com>
2023-09-25 05:36:49 +0000

    Copilot 2nd JS commit

--- AutoComplete.js
+++ AutoComplete.js
@@ -2,3 +2,9 @@ function calculateNumbers(var1, var2) {
     // 
     console.log(var1 + var2);
 }
+
+function popupmessage(title1, body1) {
+    // 
+    console.message(title1 + body1);
+    window.alert(title1 + body1);
+}
